2253f436e692d4f098cbffab1b20505818d61e13
WIN10 <pama1234@163.com>
2023-07-20 16:00:27 +0800

    update

--- JediTerm/src/main/kotlin/com/jediterm/app/JediTerm.java
+++ JediTerm/src/main/kotlin/com/jediterm/app/JediTerm.java
@@ -33,14 +33,11 @@ import com.jediterm.ui.AbstractTerminalFrame;
 import com.pty4j.PtyProcess;
 import com.pty4j.PtyProcessBuilder;
 
-import kotlin.Metadata;
 import kotlin.Pair;
 import kotlin.collections.ArraysKt;
 import kotlin.jvm.functions.Function1;
-import kotlin.jvm.internal.Intrinsics;
 import kotlin.text.Charsets;
 
-@Metadata(mv= {1,9,0},k=1,d1= {"\u0000&\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\u0018\u00002\u00020\u0001:\u0001\u000bB\u0005¢\u0006\u0002\u0010\u0002J\b\u0010\u0003\u001a\u00020\u0004H\u0014J\u0010\u0010\u0005\u001a\u00020\u00062\u0006\u0010\u0007\u001a\u00020\bH\u0014J\b\u0010\t\u001a\u00020\nH\u0016¨\u0006\f"},d2= {"Lcom/jediterm/app/JediTerm;","Lcom/jediterm/ui/AbstractTerminalFrame;","()V","createTabbedTerminalWidget","Lcom/jediterm/app/JediTabbedTerminalWidget;","createTerminalWidget","Lcom/jediterm/app/JediTerminalWidget;","settingsProvider","Lcom/jediterm/terminal/ui/settings/TabbedSettingsProvider;","createTtyConnector","Lcom/jediterm/terminal/TtyConnector;","LoggingPtyProcessTtyConnector","JediTerm"})
 public final class JediTerm extends AbstractTerminalFrame{
   @NotNull
   protected JediTabbedTerminalWidget createTabbedTerminalWidget() {
@@ -63,7 +60,7 @@ public final class JediTerm extends AbstractTerminalFrame{
       public JediTerminalWidget createInnerTerminalWidget() {
         JediTerm var10000=JediTerm.this;
         TabbedSettingsProvider var10001=this.getSettingsProvider();
-        Intrinsics.checkNotNullExpressionValue(var10001,"settingsProvider");
+        // Intrinsics.checkNotNullExpressionValue(var10001,"settingsProvider");
         return var10000.createTerminalWidget(var10001);
       }
       //  // $FF: synthetic method
@@ -95,16 +92,16 @@ public final class JediTerm extends AbstractTerminalFrame{
         if(var7==null) {
           var7="/bin/bash";
         }
-        Intrinsics.checkNotNullExpressionValue(var7,"envs[\"SHELL\"] ?: \"/bin/bash\"");
+        // Intrinsics.checkNotNullExpressionValue(var7,"envs[\"SHELL\"] ?: \"/bin/bash\"");
         String shell=var7;
         var10000=UIUtil.isMac?new String[] {shell,"--login"}:new String[] {shell};
       }
       String[] command=var10000;
       AbstractTerminalFrame.LOG.info("Starting "+command+" "+(CharSequence)null+" "+(CharSequence)null+" "+(CharSequence)null+" "+0+" "+(CharSequence)null+" "+(Function1)null+" "+63,(Object)null);
       PtyProcess var8=(new PtyProcessBuilder()).setCommand(command).setEnvironment((Map)envs).setConsole(false).setUseWinConPty(true).start();
-      Intrinsics.checkNotNullExpressionValue(var8,"PtyProcessBuilder()\n  …ty(true)\n    .start()");
+      // Intrinsics.checkNotNullExpressionValue(var8,"PtyProcessBuilder()\n  …ty(true)\n    .start()");
       PtyProcess process=var8;
-      Intrinsics.checkNotNullExpressionValue(charset,"charset");
+      // Intrinsics.checkNotNullExpressionValue(charset,"charset");
       return (TtyConnector)(new LoggingPtyProcessTtyConnector(process,charset,ArraysKt.toList(command)));
     }catch(Exception var5) {
       throw new IllegalStateException((Throwable)var5);
@@ -112,7 +109,7 @@ public final class JediTerm extends AbstractTerminalFrame{
   }
   @NotNull
   protected JediTerminalWidget createTerminalWidget(@NotNull TabbedSettingsProvider settingsProvider) {
-    Intrinsics.checkNotNullParameter(settingsProvider,"settingsProvider");
+    // Intrinsics.checkNotNullParameter(settingsProvider,"settingsProvider");
     JediTerminalWidget widget=new JediTerminalWidget((SettingsProvider)settingsProvider);
     widget.addHyperlinkFilter((HyperlinkFilter)(new UrlFilter()));
     return widget;
@@ -122,9 +119,6 @@ public final class JediTerm extends AbstractTerminalFrame{
   //  public JediTermWidget createTerminalWidget(TabbedSettingsProvider var1) {
   //     return (JediTermWidget)this.createTerminalWidget(var1);
   //  }
-  @Metadata(mv= {1,9,0},k=1,d1= {
-    "\u0000V\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010 \n\u0002\u0010\u000e\n\u0002\b\u0002\n\u0002\u0010\b\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\u0010\u0019\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\b\n\u0002\u0010\u0002\n\u0002\b\u0003\n\u0002\u0010\u0012\n\u0002\b\u0002\u0018\u00002\u00020\u00012\u00020\u0002B#\u0012\u0006\u0010\u0003\u001a\u00020\u0004\u0012\u0006\u0010\u0005\u001a\u00020\u0006\u0012\f\u0010\u0007\u001a\b\u0012\u0004\u0012\u00020\t0\b¢\u0006\u0002\u0010\nJ\u000e\u0010\u0015\u001a\b\u0012\u0004\u0012\u00020\u00100\bH\u0016J\b\u0010\u0016\u001a\u00020\fH\u0016J\u000e\u0010\u0017\u001a\b\u0012\u0004\u0012\u00020\u00120\bH\u0016J \u0010\u0018\u001a\u00020\f2\u0006\u0010\u0019\u001a\u00020\u00102\u0006\u0010\u001a\u001a\u00020\f2\u0006\u0010\u001b\u001a\u00020\fH\u0016J\u000e\u0010\u001c\u001a\u00020\u001d2\u0006\u0010\u001e\u001a\u00020\u0014J\u0010\u0010\u001f\u001a\u00020\u001d2\u0006\u0010 \u001a\u00020!H\u0016J\u0010\u0010\u001f\u001a\u00020\u001d2\u0006\u0010\"\u001a\u00020\tH\u0016R\u000e\u0010\u000b\u001a\u00020\fX\u0082D¢\u0006\u0002\n\u0000R\u000e\u0010\r\u001a\u00020\fX\u0082\u000e¢\u0006\u0002\n\u0000R\u0014\u0010\u000e\u001a\b\u0012\u0004\u0012\u00020\u00100\u000fX\u0082\u0004¢\u0006\u0002\n\u0000R\u0014\u0010\u0011\u001a\b\u0012\u0004\u0012\u00020\u00120\u000fX\u0082\u0004¢\u0006\u0002\n\u0000R\u0010\u0010\u0013\u001a\u0004\u0018\u00010\u0014X\u0082\u000e¢\u0006\u0002\n\u0000¨\u0006#"},d2= {
-      "Lcom/jediterm/app/JediTerm$LoggingPtyProcessTtyConnector;","Lcom/jediterm/pty/PtyProcessTtyConnector;","Lcom/jediterm/terminal/LoggingTtyConnector;","process","Lcom/pty4j/PtyProcess;","charset","Ljava/nio/charset/Charset;","command","","","(Lcom/pty4j/PtyProcess;Ljava/nio/charset/Charset;Ljava/util/List;)V","MAX_LOG_SIZE","","logStart","myDataChunks","Ljava/util/LinkedList;","","myStates","Lcom/jediterm/terminal/LoggingTtyConnector$TerminalState;","myWidget","Lcom/jediterm/terminal/ui/JediTermWidget;","getChunks","getLogStart","getStates","read","buf","offset","length","setWidget","","widget","write","bytes","","string","JediTerm"})
   public static final class LoggingPtyProcessTtyConnector extends PtyProcessTtyConnector implements LoggingTtyConnector{
     private final int MAX_LOG_SIZE;
     private final LinkedList myDataChunks;
@@ -132,19 +126,19 @@ public final class JediTerm extends AbstractTerminalFrame{
     private JediTermWidget myWidget;
     private int logStart;
     public int read(@NotNull char[] buf,int offset,int length) throws IOException {
-      Intrinsics.checkNotNullParameter(buf,"buf");
+      // Intrinsics.checkNotNullParameter(buf,"buf");
       int len=super.read(buf,offset,length);
       if(len>0) {
         char[] arr=ArraysKt.copyOfRange(buf,offset,len);
         this.myDataChunks.add(arr);
         JediTermWidget var10000=this.myWidget;
-        Intrinsics.checkNotNull(var10000);
+        // Intrinsics.checkNotNull(var10000);
         TerminalTextBuffer terminalTextBuffer=var10000.getTerminalTextBuffer();
-        Intrinsics.checkNotNullExpressionValue(terminalTextBuffer,"terminalTextBuffer");
+        // Intrinsics.checkNotNullExpressionValue(terminalTextBuffer,"terminalTextBuffer");
         String var10002=terminalTextBuffer.getScreenLines();
         String var10003=terminalTextBuffer.getStyleLines();
         LinesBuffer var10004=terminalTextBuffer.getHistoryBuffer();
-        Intrinsics.checkNotNullExpressionValue(var10004,"terminalTextBuffer.historyBuffer");
+        // Intrinsics.checkNotNullExpressionValue(var10004,"terminalTextBuffer.historyBuffer");
         LoggingTtyConnector.TerminalState terminalState=new LoggingTtyConnector.TerminalState(var10002,var10003,var10004.getLines());
         this.myStates.add(terminalState);
         if(this.myDataChunks.size()>this.MAX_LOG_SIZE) {
@@ -167,21 +161,21 @@ public final class JediTerm extends AbstractTerminalFrame{
       return this.logStart;
     }
     public void write(@NotNull String string) throws IOException {
-      Intrinsics.checkNotNullParameter(string,"string");
+      // Intrinsics.checkNotNullParameter(string,"string");
       AbstractTerminalFrame.LOG.debug("Writing in OutputStream : "+string);
       super.write(string);
     }
     public void write(@NotNull byte[] bytes) throws IOException {
-      Intrinsics.checkNotNullParameter(bytes,"bytes");
+      // Intrinsics.checkNotNullParameter(bytes,"bytes");
       Logger var10000=AbstractTerminalFrame.LOG;
       StringBuilder var10001=(new StringBuilder()).append("Writing in OutputStream : ");
       String var10002=Arrays.toString(bytes);
-      Intrinsics.checkNotNullExpressionValue(var10002,"toString(this)");
+      // Intrinsics.checkNotNullExpressionValue(var10002,"toString(this)");
       var10000.debug(var10001.append(var10002).append(" ").append(new String(bytes,Charsets.UTF_8)).toString());
       super.write(bytes);
     }
     public final void setWidget(@NotNull JediTermWidget widget) {
-      Intrinsics.checkNotNullParameter(widget,"widget");
+      // Intrinsics.checkNotNullParameter(widget,"widget");
       this.myWidget=widget;
     }
     public LoggingPtyProcessTtyConnector(@NotNull PtyProcess process,@NotNull Charset charset,@NotNull List command) {
--- JediTerm/src/main/kotlin/com/jediterm/app/JediTermMain.java
+++ JediTerm/src/main/kotlin/com/jediterm/app/JediTermMain.java
@@ -15,19 +15,15 @@ import javax.swing.SwingUtilities;
 
 import org.jetbrains.annotations.NotNull;
 
-import kotlin.Metadata;
 import kotlin.Unit;
 import kotlin.jvm.JvmStatic;
-import kotlin.jvm.internal.Intrinsics;
-import kotlin.text.Charsets;
 
-@Metadata(mv= {1,9,0},k=1,d1= {"\u0000 \n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0002\n\u0002\u0010\u0002\n\u0002\b\u0002\n\u0002\u0010\u0011\n\u0002\u0010\u000e\n\u0002\b\u0002\bÆ\u0002\u0018\u00002\u00020\u0001B\u0007\b\u0002¢\u0006\u0002\u0010\u0002J\b\u0010\u0003\u001a\u00020\u0004H\u0002J\u001b\u0010\u0005\u001a\u00020\u00042\f\u0010\u0006\u001a\b\u0012\u0004\u0012\u00020\b0\u0007H\u0007¢\u0006\u0002\u0010\t¨\u0006\n"},d2= {"Lcom/jediterm/app/JediTermMain;","","()V","configureJavaUtilLogging","","main","arg","","","([Ljava/lang/String;)V","JediTerm"})
 public final class JediTermMain{
   @NotNull
   public static final JediTermMain INSTANCE;
   @JvmStatic
   public static final void main(@NotNull String[] arg) {
-    Intrinsics.checkNotNullParameter(arg,"arg");
+    // Intrinsics.checkNotNullParameter(arg,"arg");
     INSTANCE.configureJavaUtilLogging();
     SwingUtilities.invokeLater((Runnable)null);
   }
@@ -35,9 +31,9 @@ public final class JediTermMain{
     String format="[%1$tF %1$tT] [%4\\$-7s] %5$s %n";
     LogManager var10000=LogManager.getLogManager();
     String var2="java.util.logging.SimpleFormatter.format="+format;
-    Charset var3=Charsets.UTF_8;
+    Charset var3=Charset.forName("UTF-8");
     byte[] var10003=var2.getBytes(var3);
-    Intrinsics.checkNotNullExpressionValue(var10003,"this as java.lang.String).getBytes(charset)");
+    // Intrinsics.checkNotNullExpressionValue(var10003,"this as java.lang.String).getBytes(charset)");
     try {
       var10000.readConfiguration((InputStream)(new ByteArrayInputStream(var10003)));
     }catch(SecurityException e) {
@@ -51,7 +47,7 @@ public final class JediTermMain{
     var9.setLevel(Level.ALL);
     Unit var7=Unit.INSTANCE;
     rootLogger.addHandler((Handler)var9);
-    Intrinsics.checkNotNullExpressionValue(rootLogger,"rootLogger");
+    // Intrinsics.checkNotNullExpressionValue(rootLogger,"rootLogger");
     rootLogger.setLevel(Level.INFO);
   }
   private JediTermMain() {}
